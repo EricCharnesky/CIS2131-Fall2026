d66f4c13c2191dca8aad0c3a19f10e06900d1885
Eric Charnesky <EricCharnesky@gmail.com>
2026-09-08 18:14:23 -0400

    chapter 2 started

---
+++ Chapter2/main.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+
+print(name)
+
+age = int(input("Enter your age: "))
+
+age = age + 1
+print(f'{name} on your birthday you will be {age}')
+
+height = float(input("How many inches tall are you? "))
